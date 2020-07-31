899e604fbeec3b17a6de5a4d9ba1e55a0ab88683
EShchemelev <eshchemelev@EShchemelevs-MacBook-Pro.local>
2020-07-31 11:35:50 +0300

    date fns

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import './app.css';
+import moment from 'moment';
+import addDays from 'date-fns/addDays'
 import styles from './app.module.css';
 
 function App() {
+    const add1New = () => 2 + 2;
+    console.log('App -> moment', moment().add('1', 'days').toDate());
+    console.log('App -> fns', addDays(new Date(), 1));
     return (
         <>
             <div>Тоже new, хули</div>
